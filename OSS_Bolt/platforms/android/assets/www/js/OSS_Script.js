5f935b6df478838c1162f0bf16a644d2045b04a3
istammar <ammar@innostark.com>
2014-10-27 11:27:14 +0500

    Commit By Ammar

--- OSS_Bolt/platforms/android/assets/www/js/OSS_Script.js
+++ OSS_Bolt/platforms/android/assets/www/js/OSS_Script.js
@@ -160,49 +160,30 @@ function LoadUserActivitiesDetail() {
     });
 }
 
-//function PopulateActivities() {
-//    var activities = 0;
-//    $.ajax({
-//        url: baseURL + "/Api/Activity?userId="+ window.localStorage.getItem("UserId"),
-//        dataType: "json",
-//        //data: { term: request.term },
-//        success: function (data) {
+function PopulateActivities() {
+    var activities = 0;
+    $.ajax({
+        url: baseURL + "/Api/Activity?userId="+ window.localStorage.getItem("UserId"),
+        dataType: "json",
+        //data: { term: request.term },
+        success: function (data) {
             
-//            activities = $.map(data, function (item) {
-//                return {
-//                    label: item.Name,
-//                    value: item.Id
-//                };
-//            });
-//            $('#select-choice-1').empty();
-//            var seloption = "<option value='none'>---Select---</option>";
-//            $.each(activities, function (i) {
-//                seloption += '<option value="' + activities[i].value + '">' + activities[i].label + '</option>';
-//            });
-//            $('#select-choice-1').append(seloption);
-//            $("#select-choice-1").prop("selectedIndex", 0);
-//            ValidateHideBusyIndicator();
-//            //$.each(activities, function (item) {
-//            //    $('#select-choice-1')
-//            //        .append($("<option></option>")
-//            //        .attr("value", activities[i].Id)
-//            //        .text(activities[i].Name));
-//            //    i++;
-//            //});
-//        }
-//    });
-
-//    //$("#select-choice-1").autocomplete({
-//    //    source: function (req, response) {
-//    //        var re = $.ui.autocomplete.escapeRegex(req.term);
-//    //        var matcher = new RegExp("^" + re, "i");
-//    //        response($.grep(activities, function (item) {
-//    //            return matcher.test(item.label) || matcher.test(normalize(item.label));
-//    //        }));
-//    //    },
-//    //    minLength: 1
-//    //});
-//}
+            activities = $.map(data, function (item) {
+                return {
+                    label: item.Name,
+                    value: item.Id
+                };
+            });
+            $('#select-choice-1').empty();
+            var seloption = "<option value='none'>---Select---</option>";
+            $.each(activities, function (i) {
+                seloption += '<option value="' + activities[i].value + '">' + activities[i].label + '</option>';
+            });
+            $('#select-choice-1').append(seloption);
+            $("#select-choice-1").prop("selectedIndex", 0);
+        }
+    });
+}
 
 function IsFBPost() {
     if ($("#chkbFB").is(":checked")) {
@@ -225,12 +206,12 @@ function IsFBPost() {
     
 ////});
 
-//function logoutUser() {
-//    window.localStorage.clear();
-//    $('#result').empty();
-//    window.location.hash = 'loginPage';
-//    $.mobile.initializePage();
-//}
+function logoutUser() {
+    window.localStorage.clear();
+    $('#result').empty();
+    window.location.hash = 'loginPage';
+    $.mobile.initializePage();
+}
 //function closeApp() {
 //    navigator.app.exitApp();
 //}
@@ -243,154 +224,143 @@ function IsFBPost() {
 //    loadLogActivity();
 //}
 
-//function ShowBusyIndicator() {
-//    $.blockUI.defaults.css.width = '70%';
-//    $.blockUI.defaults.css.left = '15%';
-//    $.blockUI.defaults.css.backgroundColor = "gray";
-//    $.blockUI.defaults.css.border = '3px solid #01bca7',
-//    $.blockUI({ message: '<img src="img/busy.gif" /> Just a moment...' });
 
-//}
-//function ValidateHideBusyIndicator() {
-//        $.unblockUI();
-//}
+// Camera
 
-//// Camera
-
-//var pictureSource;   // picture source
-//var destinationType; // sets the format of returned value
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value
 
 
 
-//// device APIs are available
-////
-//function onDeviceReady() {
-//    pictureSource = navigator.camera.PictureSourceType;
-//    destinationType = navigator.camera.DestinationType;
+// device APIs are available
+//
+function onDeviceReady() {
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
     
-//}
+}
 
-//// Called when a photo is successfully retrieved
-////
-//function onPhotoDataSuccess(imageData) {
-//    // Uncomment to view the base64-encoded image data
-//    // console.log(imageData);
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+    // Uncomment to view the base64-encoded image data
+    // console.log(imageData);
 
-//    // Get image handle
-//    //
-//    var largeImage = document.getElementById('largeImage');
+    // Get image handle
+    //
+    var largeImage = document.getElementById('largeImage');
 
-//    // Unhide image elements
-//    //
-//    largeImage.style.display = 'block';
+    // Unhide image elements
+    //
+    largeImage.style.display = 'block';
 
-//    // Show the captured photo
-//    // The inline CSS rules are used to resize the image
-//    //
-//    largeImage.src = "data:image/jpeg;base64," + imageData;
-//}
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    largeImage.src = "data:image/jpeg;base64," + imageData;
+}
 
-//// Called when a photo is successfully retrieved
-////
-//function onPhotoURISuccess(imageURI) {
-//    // Uncomment to view the image file URI
-//    // console.log(imageURI);
+// Called when a photo is successfully retrieved
+//
+function onPhotoURISuccess(imageURI) {
+    // Uncomment to view the image file URI
+    // console.log(imageURI);
 
-//    // Get image handle
-//    //
-//    var largeImage = document.getElementById('largeImage');
+    // Get image handle
+    //
+    var largeImage = document.getElementById('largeImage');
 
-//    // Unhide image elements
-//    //
-//    largeImage.style.display = 'block';
+    // Unhide image elements
+    //
+    largeImage.style.display = 'block';
 
-//    // Show the captured photo
-//    // The inline CSS rules are used to resize the image
-//    //
-//    largeImage.src = imageURI;
-//}
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    largeImage.src = imageURI;
+}
 
-//// A button will call this function
-////
-//function capturePhoto() {
-//    // Take picture using device camera and retrieve image as base64-encoded string
-//    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-//        quality: 50,
-//        destinationType: destinationType.DATA_URL
-//    });
-//}
+// A button will call this function
+//
+function capturePhoto() {
+    // Take picture using device camera and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+        quality: 50,
+        destinationType: destinationType.DATA_URL
+    });
+}
 
-//// A button will call this function
-////
-//function capturePhotoEdit() {
-//    // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-//    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-//        quality: 20, allowEdit: true,
-//        destinationType: destinationType.DATA_URL
-//    });
-//}
+// A button will call this function
+//
+function capturePhotoEdit() {
+    // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+        quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL
+    });
+}
 
-//// A button will call this function
-////
-//function getPhoto(source) {
-//    // Retrieve image file location from specified source
-//    navigator.camera.getPicture(onPhotoURISuccess, onFail, {
-//        quality: 50,
-//        destinationType: destinationType.FILE_URI,
-//        sourceType: source
-//    });
-//}
+// A button will call this function
+//
+function getPhoto(source) {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+        quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source
+    });
+}
 
-//// Called if something bad happens.
-////
-//function onFail(message) {
-//    console.log(message);
-//    alert('Failed because: ' + message);
-//}
+// Called if something bad happens.
+//
+function onFail(message) {
+    console.log(message);
+    alert('Failed because: ' + message);
+}
 
-//function saveActivityLog() {
+function saveActivityLog() {
     
-//    if ($("#chkbFB").is(":checked")) {
-//        if (validateActivityLogFb()) {
-//            saveActivityLogFb();
-//        }
-//    } else {
-//        if (validateActivityLogNoFb()) {
-//            saveActivityLogNoFb();
-//        }
-//    }
-//}
-//var imageURI = document.getElementById('largeImage').getAttribute("src");
+    if ($("#chkbFB").is(":checked")) {
+        if (validateActivityLogFb()) {
+            //saveActivityLogFb();
+        }
+    } else {
+        if (validateActivityLogNoFb()) {
+            saveActivityLogNoFb();
+        }
+    }
+}
+var imageURI = document.getElementById('largeImage').getAttribute("src");
 
-//function validateActivityLogFb() {
-//    if ($("#select-choice-1").val() == "none" || $('#comment').val() == '' || $("#date").val() == "") {
-//        alert("All fields are mendatory");
-//        return false;
-//    }
-//    // Validate date <= today
+function validateActivityLogFb() {
+    if ($("#select-choice-1").val() == "none" || $('#comment').val() == '' || $("#date").val() == "") {
+        alert("All fields are mendatory");
+        return false;
+    }
+    // Validate date <= today
     
-//    if (!imageURI) {
-//        alert('Please select an image first.');
-//        return false;
-//    }
-//    return true;
-//}
+    if (!imageURI) {
+        alert('Please select an image first.');
+        return false;
+    }
+    return true;
+}
 
-//function validateActivityLogNoFb() {
-//    if ($("#select-choice-1").val() == "none" || $("#date").val() == "") {
-//        alert("Please select Date and Activity");
-//        return false;
-//    }
-//    // Validate date <= today
-//    var ctlDate = Date.parse($("#date").val());
-//    var dt = new Date();
-//    //var currDate = dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + dt.getDate();;
-//    if (ctlDate > Date.parse(dt)) {
-//        alert("Select a correct date");
-//        return false;
-//    }
-//    return true;
-//}
+function validateActivityLogNoFb() {
+    if ($("#select-choice-1").val() == "none" || $("#date").val() == "") {
+        alert("Please select Date and Activity");
+        return false;
+    }
+    // Validate date <= today
+    var ctlDate = Date.parse($("#date").val());
+    var dt = new Date();
+    //var currDate = dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + dt.getDate();;
+    if (ctlDate > Date.parse(dt)) {
+        alert("Select a correct date");
+        return false;
+    }
+    return true;
+}
 
 //function saveActivityLogFb() {
 //    //"ActivityId": $("#select-choice-1").val(),       "Date": $("#date").val(),"UploadImage": imageURI,"IsFbPost": $("#chkbFB").val()
@@ -439,46 +409,46 @@ function IsFBPost() {
 //    //ft.upload(imageURI, encodeURI("http://some.server.com/upload.php"), win, fail, options);
 //}
 
-//function saveActivityLogNoFb() {
+function saveActivityLogNoFb() {
 
-//    var isFbPost = $("#chkbFB").is(':checked');
-//    var actLogData = {
-//        "ActivityId": $("#select-choice-1").val(),
-//        "UserId": window.localStorage.getItem("UserId"),
-//        "Date": $("#date").val(),
-//        "IsFbPost": isFbPost
-//    };
+    var isFbPost = $("#chkbFB").is(':checked');
+    var actLogData = {
+        "ActivityId": $("#select-choice-1").val(),
+        "UserId": window.localStorage.getItem("UserId"),
+        "Date": $("#date").val(),
+        "IsFbPost": isFbPost
+    };
 
-//    $.ajax({
-//        type: "POST",
-//        url: baseURL + '/Api/UserActivity',
-//        async: true,
-//        cache: false,
-//        data: actLogData,
-//        contentType: 'application/x-www-form-urlencoded; charset=UTF-8', //When sending data to the server
-//        dataType: 'json',  //The type of data that you're expecting back from the server.
-//        success: saveSuccess
+    $.ajax({
+        type: "POST",
+        url: baseURL + '/Api/UserActivity',
+        async: true,
+        cache: false,
+        data: actLogData,
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8', //When sending data to the server
+        dataType: 'json',  //The type of data that you're expecting back from the server.
+        success: saveSuccess
          
-//    });
-//}
+    });
+}
 
-//function saveSuccess(data) {
-//    if (data) {
-//        alert("Success");
-//    } else {
-//        alert("Fail");
-//    }
-//}
+function saveSuccess(data) {
+    if (data) {
+        alert("Success");
+    } else {
+        alert("Fail");
+    }
+}
 
-//function win(r) {
-//    console.log("Code = " + r.responseCode);
-//    console.log("Response = " + r.response);
-//    //alert("Response =" + r.response);
-//    console.log("Sent = " + r.bytesSent);
-//}
+function win(r) {
+    console.log("Code = " + r.responseCode);
+    console.log("Response = " + r.response);
+    //alert("Response =" + r.response);
+    console.log("Sent = " + r.bytesSent);
+}
 
-//function fail(error) {
-//    alert("An error has occurred: Code = " + error.code);
-//    console.log("upload error source " + error.source);
-//    console.log("upload error target " + error.target);
-//}
+function fail(error) {
+    alert("An error has occurred: Code = " + error.code);
+    console.log("upload error source " + error.source);
+    console.log("upload error target " + error.target);
+}
